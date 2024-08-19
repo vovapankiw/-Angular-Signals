01d60d1166c8b95187277748c0776e7d7fdd6f78
Volodymyr Pankiv <vpanki@softserveinc.com>
2024-08-19 09:25:44 +0300

    initial sceleton
    Signed-off-by: Volodymyr Pankiv <vpanki@softserveinc.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,25 +2,11 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {LessonsComponent} from "./lessons/lessons.component";
-import {isUserAuthenticated} from "./guards/auth.guard";
-import {CourseComponent} from "./course/course.component";
-import {courseResolver} from "./course/course.resolver";
-import {courseLessonsResolver} from "./course/course-lessons.resolver";
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-    canActivate: [isUserAuthenticated]
-  },
-  {
-    'path': 'courses/:courseId',
-    component: CourseComponent,
-    canActivate: [isUserAuthenticated],
-    resolve: {
-      course: courseResolver,
-      lessons: courseLessonsResolver
-    }
+    component: HomeComponent
   },
   {
     path: "login",
